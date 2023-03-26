139a283d174eef65b0afbefe13ffd833a7c55e00
Rishav Thakker <thakker.rishav@gmail.com>
2023-03-26 22:21:35 +0530

    Script to generate html with spotlight divs

---
+++ fill_images.py
@@ -0,0 +1,36 @@
+import os
+root_folder = "demo/gallery/"
+
+content = ""
+
+for folder in os.listdir(root_folder):
+    folder_path = os.path.join(root_folder, folder)
+
+    parent_start = f"""
+    <div style="display: none" id="{folder}" class="spotlight-group" data-fit="cover" data-autohide="all" data-animation="fade" data-control="close">
+    """
+    parent_end = "</div>"
+
+    elements = []
+    for image in os.listdir(folder_path):
+        image_path = os.path.join(folder_path, image)
+
+        elements.append(
+            f"""
+            <a class="spotlight" href="{image_path}">
+                <img src="{image_path}">
+            </a>
+            """
+        )
+
+    parent = parent_start + "\n".join(elements) + parent_end
+
+    content += parent + "\n"
+
+with open("index.html") as infile:
+    html = infile.read()
+
+html.replace("{{content}}", content)
+
+with open("index.html", "w") as outfile:
+    outfile.write(html)
